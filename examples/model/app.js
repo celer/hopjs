48d60b1287b667b76a81174268834382b10ad40e
celer <celer@coyote.(none)>
2013-01-01 23:17:03 -0700

    Fixed a bug with the caching example, and added specific processing phases

--- examples/model/app.js
+++ examples/model/app.js
@@ -140,7 +140,7 @@ Hop.defineTestCase("UserService.create: Basic tests",function(test){
 Hop.defineTestCase("UserService.create: Advanced",function(test){
 	var validUser = { email:"test@test.com", name:"TestUser", password:"sillycat" };
 	test.do("UserService.create").with(validUser).inputSameAsOutput().saveOutputAs("createdUser");
-	test.do("UserService.create").with({name:undefined},validUser).errorContains("parameter 'name'");
+	test.do("UserService.create").with({name:undefined},validUser).errorContains("Missing parameter");
 	test.do("UserService.create").with({email:"X"},validUser).errorContains("Invalid email");
 	test.do("UserService.create").with({name:"@#$"},validUser).errorContains("alphanumeric");
 
