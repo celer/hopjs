bc463c6b19eb50da948db9b28b48c0bb1a14b2f3
pair <pair@slipangle.us>
2013-01-21 06:01:14 +0000

    Added CSRF and secure example

--- examples/secure/README.md
+++ examples/secure/README.md
@@ -15,31 +15,13 @@ Now visit http://localhost:3000/ in your browser
 
 *Next steps - Things to try doing with the example:*
 
-1. Visit the documentation page and run all the examples
+1. Visit the documentation page and run all the test cases 
 
-2. Open the JavaScript console in your browser and use the API 
- 
-```javascript
-   UserService.create({ name: "user", email:"user@site.com", password:"foofoo"},function(err,result){ console.log(err,result}; });
-```
+2. Visit the same website using the https URL 
 
-3. Generate the shell script stub
+i.e. https://loalhost:3443/
 
-```shell
-  # This will generate a shell script which uses 'curl' to call our API
-  ../../bin/hopjs-gen --url http://localhost:3000/ shell --output api.sh
-  chmod 755 ./api.sh
-
-  # Now let's use the command line to create a user
-  ./api.sh UserService.create --APIURL http://localhost:3000/ --email user@site.com --password foofoo --name user
-  
-  # Now we'll generate the unit test for this stub
-  ../../bin/hopjs-gen --url http://localhost:3000/ shell --unitTest --output test_api.sh 
-  chmod 755 ./test_api.sh
-
-  ./test_api.sh http://localhost:3000/ ./api.sh
-
-```
+3. Submit the login form 
 
 *Hints*
 
--- examples/secure/app.js
+++ examples/secure/app.js
@@ -3,9 +3,13 @@ var path = require('path');
 var https = require('https');
 var http = require('http');
 var fs = require('fs');
+var os = require('os');
 
 var Hop = require("./../../index");
 
+/* 
+	Set up our certificates
+*/
 var httpsOptions = {
 	key: fs.readFileSync('key.pem'),
 	cert: fs.readFileSync('cert.pem'),
@@ -14,39 +18,67 @@ var httpsOptions = {
 
 
 /*
-  This is express boiler plate, see http://expressjs.com/guide.html
+	This is express boiler plate, see http://expressjs.com/guide.html
 */
 var app = express();
 app.configure(function(){
-  app.set('port', process.env.PORT || 3000);
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(express.cookieParser('your secret here'));
-  app.use(express.session());
-  app.use(app.router);
-  app.use(express.static(path.join(__dirname, 'public')));
+	app.set('port', process.env.PORT || 3000);
+	app.set('views', __dirname + '/views');
+	app.set('view engine', 'jade');
+	app.use(express.bodyParser());
+	app.use(express.methodOverride());
+	app.use(express.cookieParser('your secret here'));
+	app.use(express.session());
+
+	/*
+		This will tell express to use it's csrf protection: ( http://en.wikipedia.org/wiki/Cross-site_request_forgery )
+	*/	
+	app.use(express.csrf());
+	app.use(app.router);
+	app.use(express.static(path.join(__dirname, 'public')));
 });
 
 app.engine("jade",require('jade').__express);
 
 app.get("/",function(req,res){
-	res.render("index");
+	res.render("index",{ _csrf: req.session._csrf, hostname: os.hostname() });
 });
 
-SecureService={
-	testCall: function(input,onComplete,req){
+SecureService={	
+	csrf: function(input,onComplete,req){
+		return onComplete(null,true);
+	},
+	secureCall: function(input,onComplete,req){
+		return onComplete(null,input);
+	},
+	insecureCall: function(input,onComplete,req){
 		return onComplete(null,input);
 	}
 }
 
 Hop.defineClass("SecureService",SecureService,function(api){
-	api.post("testCall","/secure").demand("username").demand("password").secure();
+	/* 
+		We will use this call to get the csrf token for the test scripts - this is generally a really bad idea and defeats the purpose express's CSRF protection	
+		
+		In a production application a better solution would be to use CSRF protection for all posts and then use an express middleware wrapper	
+		to allow the authentication call to be called without a CSRF token and have it return the token so scripting tools may use the 
+		CSRF token 
+	 	
+	*/
+	api.get("csrf","/csrf").sendCSRFToken();
+	/* This call will only work over HTTPS */
+	api.post("secureCall","/secure").demand("username").demand("password").sendCSRFToken().secure();
+	api.post("insecureCall","/insecure").demand("username").demand("password").sendCSRFToken();
 });
 
-Hop.defineTestCase("SecureService.testCall",function(test){
-	test.do("SecureService.testCall").with({ username: "bob", password:"foo"}).errorContains("Permission denied");
+Hop.defineTestCase("SecureService.secureCall",function(test){
+	test.do("SecureService.csrf").with({}).noError();
+	test.do("SecureService.secureCall").with({ username: "bob", password:"foo"}).errorContains("Permission denied");
+});
+
+Hop.defineTestCase("SecureService.insecureCall",function(test){
+	test.do("SecureService.csrf").with({}).noError();
+	test.do("SecureService.insecureCall").with({ username: "bob", password:"foo"}).inputSameAsOutput();;
 });
 
 Hop.apiHook("/api/",app);
