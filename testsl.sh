6332b70624eb80f0445837c3ca2aa52d25db52d5
celer <celer@coyote.local>
2014-03-10 19:17:12 -0600

    Updated to work with SL

--- testsl.sh
+++ testsl.sh
@@ -6,7 +6,7 @@ sleep 1
 
 HOSTNAME=`hostname -f`
 
-node_modules/hopjs-remote/bin/hopjs-selenium --url http://localhost:3000/  --browser opera --platform Linux --version 12  --remote "http://${SAUCE_USERNAME}:${SAUCE_ACCESS_KEY}@localhost:4445/wd/hub" --name "Test with opera"
+node_modules/hopjs-remote/bin/hopjs-selenium --url http://localhost:3000/  --browser opera --platform Linux --version 12  --remote "http://${SAUCE_USERNAME}:${SAUCE_ACCESS_KEY}@localhost:4445/wd/hub" --name "Build: ${TRAVIS_BUILD_NUMBER}" --tunnelid ${TRAVIS_JOB_NUMBER}
 EC=$?
 
 
