2a08c703525621cbd984f0b1c10117949277b61b
celer <celer@coyote.local>
2014-03-18 17:09:37 -0600

    Cleaned up tests and attempted to get SL to test multiple browsers

--- testsl.sh
+++ testsl.sh
@@ -7,10 +7,33 @@ sleep 1
 
 HOSTNAME=`hostname -f`
 
-node_modules/hopjs-remote/bin/hopjs-selenium --url http://localhost:3000/  --browser opera --platform Linux --version 12  --remote "http://${SAUCE_USERNAME}:${SAUCE_ACCESS_KEY}@localhost:16455/wd/hub" --name "Build: ${TRAVIS_BUILD_NUMBER}" --tunnelid ${TRAVIS_JOB_NUMBER}
-EC=$?
+
+BROWSERS="internet explorer,Windows 7,9;firefox,Linux,27;chrome,Linux,32;safari,OS X 10.8,7" 
+
+IFS=';'
+for b in $BROWSERS
+do
+  IFS=','
+  set $b
+  BROWSER=$1
+  PLATFORM=$2
+  VERSION=$3
+
+   
+  if ! node_modules/hopjs-remote/bin/hopjs-selenium --url http://localhost:3000/  --browser "$BROWSER" --platform "$PLATFORM" --version "$VERSION" --remote "http://${SAUCE_USERNAME}:${SAUCE_ACCESS_KEY}@localhost:16455/wd/hub" --name "Build: ${TRAVIS_BUILD_NUMBER}" --tunnelid ${TRAVIS_JOB_NUMBER} --build ${TRAVIS_BUILD_NUMBER} --public true
+  then
+    kill $APP_PID
+    exit -1
+  fi
+  
+  
+
+  IFS=';'
+done
+
+
 
 kill $APP_PID
 
 
-exit $?
+exit 0
