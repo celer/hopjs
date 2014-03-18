509991822f1a74c3d25a0a94542dbf0c98adc15c
celer <celer@coyote.local>
2014-03-17 23:53:24 -0600

    Yet another attempt to make Sauce Labs go

--- testsl.sh
+++ testsl.sh
@@ -7,9 +7,8 @@ sleep 1
 
 HOSTNAME=`hostname -f`
 
-node_modules/hopjs-remote/bin/hopjs-selenium --url http://localhost:3000/  --browser opera --platform Linux --version 12  --remote "http://${SAUCE_USERNAME}:${SAUCE_ACCESS_KEY}@localhost:4445/wd/hub" --name "Build: ${TRAVIS_BUILD_NUMBER}" --tunnelid ${TRAVIS_JOB_NUMBER}
+node_modules/hopjs-remote/bin/hopjs-selenium --url http://localhost:3000/  --browser opera --platform Linux --version 12  --remote "http://${SAUCE_USERNAME}:${SAUCE_ACCESS_KEY}@localhost:16455/wd/hub" --name "Build: ${TRAVIS_BUILD_NUMBER}" --tunnelid ${TRAVIS_JOB_NUMBER}
 EC=$?
-
 
 kill $APP_PID
 
